a336dbabe0ee98e646d9c3ee8ce4602aaceed7bb
infcap <stalkersligo@gmail.com>
2024-09-10 22:14:50 +0300

    Add files via upload

---
+++ povtorenie/povtorenie.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main(void) {
+    std::vector<int> v = { {1,2,3,4,5} };
+    std::transform(std::begin(v), std::end(v), std::begin(v), [](int x) {return x + 5; });
+    for (auto e : v)
+    {
+        std::cout << e << std::endl;
+    }
+}
